4391cf4ed9d33492211d0b602c65c59806665aa8
willianpontes2 <107737768+willianpontes2@users.noreply.github.com>
2023-03-16 16:37:20 -0300

    First project in Java based in Capgemini's Start

---
+++ connectionFactory.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+/**
+ *
+ * @author willi
+ */
+public class connectionFactory {
+
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/WillApp";
+    public static final String USER = "root";
+    public static final String PASS = "";
+    
+}
